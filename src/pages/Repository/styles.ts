2fdf06a9c504025a6fd266ef4ebae13f51496b79
Elias <eliasmelo1978@gmail.com>
2020-05-07 23:32:51 -0300

    final commit

--- src/pages/Repository/styles.ts
+++ src/pages/Repository/styles.ts
@@ -50,7 +50,7 @@ export const RepositoryInfo = styled.section`
     }
   }
 
-  /* ul {
+  ul {
     display: flex;
     list-style: none;
     margin-top: 40px;
@@ -71,5 +71,51 @@ export const RepositoryInfo = styled.section`
       margin-top: 4px;
       color: #6c6c80;
     }
-  } */
+  }
+`;
+
+export const Issues = styled.div`
+  margin-top: 80px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+  }
+
+  div {
+    margin-left: 16px;
+    flex: 1;
+
+    strong {
+      font-size: 20px;
+      color: #3d3d4d;
+    }
+
+    p {
+      font-size: 18px;
+      color: #a8a8b3;
+      margin-top: 4px;
+    }
+  }
+
+  svg {
+    margin-left: auto;
+    color: #cbcbd6;
+  }
 `;
